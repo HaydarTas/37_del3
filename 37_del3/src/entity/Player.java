0231e22ff87b5ac83ad617c4c497dde0de18ebd2
Haydar Tas <Haydar@10.16.167.17>
2015-11-26 16:46:39 +0100

    Player klassen er klar

--- 37_del3/src/entity/Player.java
+++ 37_del3/src/entity/Player.java
@@ -5,13 +5,22 @@ public class Player {
 	private String name;
 	private Account account;
 	private boolean bankrupt = false;
+	private Field[] fields = new Field[21];
+	private int number;
+	private int position;
+	private int info; // Information about whether the field is bought or not
+	private int lastRoll;
 
-	public Player() {
-
+	public Player(String name, int number) {
+		this.name = name;
 		this.account = new Account();
+		this.account.addToBalance(30000);
+		this.number = number;
 
-		this.account.addToBalance(1000);
+	}
 
+	public int getNumber() {
+		return number;
 	}
 
 	public String getName() {
@@ -33,13 +42,69 @@ public class Player {
 	}
 
 	public boolean isBankrupt() {
-		return bankrupt ;
+		return bankrupt;
 	}
-	
+
 	/**
-	 * setBankrupt sets the bankrupt status to true. 
+	 * setBankrupt sets the bankrupt status to true.
 	 */
-	public void setBankrupt(){
+	public void setBankrupt() {
 		bankrupt = true;
 	}
+
+	public Field[] getFields() {
+		return fields;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void movePlayer(int dice) {
+		position = position + dice;
+		if (position > 21) {
+			position = position - 21;
+		}
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Player) {
+			Player tmp = (Player) obj;
+			return this.name == tmp.getName() && this.number == tmp.getNumber();
+		} else {
+			return false;
+		}
+	}
+
+	public boolean buyField(Ownable o) {
+		if (this.getBalance() > o.getPrice()) {
+			this.addToBalance(-o.getPrice());
+			for (int i = 0; i < fields.length; i++) {
+				if (fields[i] == null) {
+					fields[i] = o;
+					return true;
+				}
+			}
+
+		}
+		return false;
+	}
+
+	public int getInformation() {
+		return info;
+	}
+
+	public void setInformation(int i) {
+		info = i;
+	}
+
+	public int getLastRoll() {
+		return lastRoll;
+	}
+
+	public void setLastRoll(int diceValues) {
+		lastRoll = diceValues;
+	}
+
 }
